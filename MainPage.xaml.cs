c6ee54d1064a7ec6bbebc0a3ca51bdf1a8b9ae5c
Lucas Cardoso <lucas_cardoso_de_castro@hotmail.com>
2023-09-02 12:12:01 -0300

    Construção da lógica

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,14 +2,13 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
     public void PedraButton_Clicked(object sender, EventArgs e)
     {
-
         opcaoSelecionada(1);
     }
 
@@ -23,10 +22,50 @@ public partial class MainPage : ContentPage
         opcaoSelecionada(3);
     }
 
+    public int opcaoComputador()
+    {
+        Random random = new Random();
+        int computadorOpcao = random.Next(1, 4);
+
+        return computadorOpcao;
+    }
+
 
     public void opcaoSelecionada(int opcaoUsuario)
-	{
-        TextResultado.Text = opcaoUsuario.ToString();
+    {
+        var usuario = opcaoUsuario;
+        var computador = opcaoComputador();
+
+        if (computador == usuario)
+        {
+            TextResultado.Text = "Empate!";
+        }
+        else if (   computador == 1 && usuario == 3 ||
+                    computador == 2 && usuario == 1 ||
+                    computador == 3 && usuario == 2 )
+        {
+            TextResultado.Text = "Você perdeu a rodada!";
+        }
+        else
+        {
+            TextResultado.Text = "Você venceu a rodada!";
+        }
     }
 }
+
+/*
+1 = Pedra
+2 = Papel
+3 = Tesoura
+
+1 1 = empate
+1 2 = usuario ganha
+1 3 = computador ganha
+2 2 = empate
+2 1 = computador ganha
+2 3 = usuario ganha
+3 3 = empate
+3 1 = usuario ganha
+3 2 = computador ganha
+ */
 
